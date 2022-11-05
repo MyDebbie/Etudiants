18eaaef8fee858734893156f067038eda42e7381
Debbie <dimanchemydl133@gmail.com>
2022-11-05 12:20:24 -0400

    Add project files.

---
+++ Etudiants/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace Etudiants
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lbNom = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.lbNationalite = new System.Windows.Forms.Label();
+            this.lbPrenom1 = new System.Windows.Forms.Label();
+            this.lbAge = new System.Windows.Forms.Label();
+            this.lbPrenom2 = new System.Windows.Forms.Label();
+            this.lbVille = new System.Windows.Forms.Label();
+            this.lbAdresse = new System.Windows.Forms.Label();
+            this.lbTelephone = new System.Windows.Forms.Label();
+            this.txtPrenom2 = new System.Windows.Forms.TextBox();
+            this.txtNationalite = new System.Windows.Forms.TextBox();
+            this.txtVille = new System.Windows.Forms.TextBox();
+            this.txtPrenom1 = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lvPersonne = new System.Windows.Forms.ListView();
+            this.lbPays = new System.Windows.Forms.Label();
+            this.txtPays = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSave.Location = new System.Drawing.Point(89, 558);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(153, 51);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lbNom
+            // 
+            this.lbNom.AutoSize = true;
+            this.lbNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNom.Location = new System.Drawing.Point(31, 76);
+            this.lbNom.Name = "lbNom";
+            this.lbNom.Size = new System.Drawing.Size(59, 25);
+            this.lbNom.TabIndex = 14;
+            this.lbNom.Text = "Nom:";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(137, 80);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(202, 22);
+            this.txtNom.TabIndex = 0;
+            // 
+            // lbNationalite
+            // 
+            this.lbNationalite.AutoSize = true;
+            this.lbNationalite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNationalite.Location = new System.Drawing.Point(23, 276);
+            this.lbNationalite.Name = "lbNationalite";
+            this.lbNationalite.Size = new System.Drawing.Size(109, 25);
+            this.lbNationalite.TabIndex = 22;
+            this.lbNationalite.Text = "Nationalité:";
+            // 
+            // lbPrenom1
+            // 
+            this.lbPrenom1.AutoSize = true;
+            this.lbPrenom1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPrenom1.Location = new System.Drawing.Point(417, 84);
+            this.lbPrenom1.Name = "lbPrenom1";
+            this.lbPrenom1.Size = new System.Drawing.Size(97, 25);
+            this.lbPrenom1.TabIndex = 38;
+            this.lbPrenom1.Text = "Prénom1:";
+            // 
+            // lbAge
+            // 
+            this.lbAge.AutoSize = true;
+            this.lbAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAge.Location = new System.Drawing.Point(425, 181);
+            this.lbAge.Name = "lbAge";
+            this.lbAge.Size = new System.Drawing.Size(54, 25);
+            this.lbAge.TabIndex = 9;
+            this.lbAge.Text = "Age:";
+            // 
+            // lbPrenom2
+            // 
+            this.lbPrenom2.AutoSize = true;
+            this.lbPrenom2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPrenom2.Location = new System.Drawing.Point(23, 181);
+            this.lbPrenom2.Name = "lbPrenom2";
+            this.lbPrenom2.Size = new System.Drawing.Size(97, 25);
+            this.lbPrenom2.TabIndex = 11;
+            this.lbPrenom2.Text = "Prénom2:";
+            // 
+            // lbVille
+            // 
+            this.lbVille.AutoSize = true;
+            this.lbVille.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVille.Location = new System.Drawing.Point(23, 390);
+            this.lbVille.Name = "lbVille";
+            this.lbVille.Size = new System.Drawing.Size(55, 25);
+            this.lbVille.TabIndex = 12;
+            this.lbVille.Text = "Ville:";
+            // 
+            // lbAdresse
+            // 
+            this.lbAdresse.AutoSize = true;
+            this.lbAdresse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAdresse.Location = new System.Drawing.Point(402, 275);
+            this.lbAdresse.Name = "lbAdresse";
+            this.lbAdresse.Size = new System.Drawing.Size(91, 25);
+            this.lbAdresse.TabIndex = 13;
+            this.lbAdresse.Text = "Adresse:";
+            // 
+            // lbTelephone
+            // 
+            this.lbTelephone.AutoSize = true;
+            this.lbTelephone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTelephone.Location = new System.Drawing.Point(402, 389);
+            this.lbTelephone.Name = "lbTelephone";
+            this.lbTelephone.Size = new System.Drawing.Size(112, 25);
+            this.lbTelephone.TabIndex = 14;
+            this.lbTelephone.Text = "Téléphone:";
+            // 
+            // txtPrenom2
+            // 
+            this.txtPrenom2.Location = new System.Drawing.Point(137, 184);
+            this.txtPrenom2.Name = "txtPrenom2";
+            this.txtPrenom2.Size = new System.Drawing.Size(202, 22);
+            this.txtPrenom2.TabIndex = 2;
+            // 
+            // txtNationalite
+            // 
+            this.txtNationalite.Location = new System.Drawing.Point(138, 279);
+            this.txtNationalite.Name = "txtNationalite";
+            this.txtNationalite.Size = new System.Drawing.Size(202, 22);
+            this.txtNationalite.TabIndex = 4;
+            // 
+            // txtVille
+            // 
+            this.txtVille.Location = new System.Drawing.Point(138, 393);
+            this.txtVille.Name = "txtVille";
+            this.txtVille.Size = new System.Drawing.Size(202, 22);
+            this.txtVille.TabIndex = 6;
+            // 
+            // txtPrenom1
+            // 
+            this.txtPrenom1.Location = new System.Drawing.Point(532, 88);
+            this.txtPrenom1.Name = "txtPrenom1";
+            this.txtPrenom1.Size = new System.Drawing.Size(202, 22);
+            this.txtPrenom1.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(532, 185);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(202, 22);
+            this.txtAge.TabIndex = 3;
+            // 
+            // txtAdresse
+            // 
+            this.txtAdresse.Location = new System.Drawing.Point(532, 275);
+            this.txtAdresse.Name = "txtAdresse";
+            this.txtAdresse.Size = new System.Drawing.Size(202, 22);
+            this.txtAdresse.TabIndex = 5;
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.Location = new System.Drawing.Point(532, 389);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(202, 22);
+            this.txtTelephone.TabIndex = 7;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCancel.Location = new System.Drawing.Point(485, 558);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(153, 51);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lvPersonne
+            // 
+            this.lvPersonne.HideSelection = false;
+            this.lvPersonne.Location = new System.Drawing.Point(758, 80);
+            this.lvPersonne.Name = "listView1";
+            this.lvPersonne.Size = new System.Drawing.Size(653, 544);
+            this.lvPersonne.TabIndex = 23;
+            this.lvPersonne.UseCompatibleStateImageBehavior = false;
+            // 
+            // lbPays
+            // 
+            this.lbPays.AutoSize = true;
+            this.lbPays.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPays.Location = new System.Drawing.Point(23, 477);
+            this.lbPays.Name = "lbPays";
+            this.lbPays.Size = new System.Drawing.Size(55, 25);
+            this.lbPays.TabIndex = 39;
+            this.lbPays.Text = "Pays:";
+            // 
+            // txtPays
+            // 
+            this.txtPays.Location = new System.Drawing.Point(137, 477);
+            this.txtPays.Name = "txtPays";
+            this.txtPays.Size = new System.Drawing.Size(202, 22);
+            this.txtPays.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1437, 711);
+            this.Controls.Add(this.txtPays);
+            this.Controls.Add(this.lbPays);
+            this.Controls.Add(this.lvPersonne);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtTelephone);
+            this.Controls.Add(this.txtAdresse);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtPrenom1);
+            this.Controls.Add(this.txtVille);
+            this.Controls.Add(this.txtNationalite);
+            this.Controls.Add(this.txtPrenom2);
+            this.Controls.Add(this.lbTelephone);
+            this.Controls.Add(this.lbAdresse);
+            this.Controls.Add(this.lbVille);
+            this.Controls.Add(this.lbPrenom2);
+            this.Controls.Add(this.lbAge);
+            this.Controls.Add(this.lbPrenom1);
+            this.Controls.Add(this.lbNationalite);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.lbNom);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lbNom;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label lbNationalite;
+        private System.Windows.Forms.Label lbPrenom1;
+        private System.Windows.Forms.Label lbAge;
+        private System.Windows.Forms.Label lbPrenom2;
+        private System.Windows.Forms.Label lbVille;
+        private System.Windows.Forms.Label lbAdresse;
+        private System.Windows.Forms.Label lbTelephone;
+        private System.Windows.Forms.TextBox txtPrenom2;
+        private System.Windows.Forms.TextBox txtNationalite;
+        private System.Windows.Forms.TextBox txtVille;
+        private System.Windows.Forms.TextBox txtPrenom1;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtAdresse;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListView lvPersonne;
+        private System.Windows.Forms.Label lbPays;
+        private System.Windows.Forms.TextBox txtPays;
+    }
+}
+
---
+++ Etudiants/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// @Author: Mydleyka Dimanche
+/// </summary>
+
+namespace Etudiants
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Save Information to the file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtNom.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Nom Manquant!");
+                return;
+            }
+            if (txtPrenom1.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Prénom1 Manquant!");
+                return;
+            }
+            if (txtPrenom1.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Prénom1 Manquant!");
+                return;
+            }
+            if (txtPrenom1.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Prénom1 Manquant!");
+                return;
+            }
+            if (txtPrenom1.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Prénom1 Manquant!");
+                return;
+            }
+            if (txtPrenom2.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Prénom2 Manquant!");
+                return;
+            }
+            if (txtAge.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Age Manquant!");
+                return;
+            }
+            if (txtAdresse.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Adresse Manquant!");
+                return;
+            }
+            if (txtVille.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Ville Manquant!");
+                return;
+            }
+            if (txtTelephone.Text.Trim() == String.Empty)
+            {
+                MessageBox.Show("Telephone Manquant!");
+                return;
+            }
+
+            List<Personne> Personnes = new List<Personne> { };
+
+
+            Personne personne = new Personne(txtNom.Text, txtPrenom1.Text, txtPrenom2.Text, int.Parse(txtAge.Text), txtPays.Text, txtNationalite.Text, txtTelephone.Text, txtVille.Text, txtAdresse.Text,DateTime.Now.ToString("d"));
+
+            Personnes.Add(personne);
+
+
+          
+
+            // Mettre dans Fichier
+            FileInfo fileInfo = new FileInfo(@"personnes.txt");
+
+            if (!fileInfo.Exists)
+            {
+                fileInfo.Create();
+            }
+            else { 
+
+            using (StreamWriter sw = new StreamWriter(@"personnes.txt", true, Encoding.UTF8))
+            {
+                sw.WriteLine(personne.Nom + " " + personne.Prenom1 + " " + personne.Prenom2 + " " + personne.Age + " " + personne.Pays + " " + personne.Nationalite + " " + personne.Telephone + " " + personne.Ville + " " + personne.AdresseRue + " " + personne.Datecree);
+            }
+            }
+
+            ListViewItem listviewItem = new ListViewItem();
+            listviewItem.Text = personne.Nom;
+            listviewItem.SubItems.Add(personne.Prenom1 + " " + personne.Prenom2);
+            listviewItem.SubItems.Add(txtAge.Text);
+            listviewItem.SubItems.Add(txtTelephone.Text);
+
+            lvPersonne.Items.Add(listviewItem);
+        }
+
+        /// <summary>
+        /// Clear all the boxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtNom.Text = String.Empty;
+            txtPrenom1.Text = String.Empty;
+            txtPrenom2.Text = String.Empty;
+            txtAge.Text = String.Empty;
+            txtVille.Text = String.Empty;
+            txtAdresse.Text = String.Empty;
+            txtTelephone.Text = String.Empty;
+            txtNationalite.Text = String.Empty;
+            txtPays.Text = String.Empty;
+
+        }
+        /// <summary>
+        /// Load the information of the file to the screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lvPersonne.View = View.Details;
+            lvPersonne.Clear();
+            lvPersonne.Columns.Add("Nom", 150, HorizontalAlignment.Left);
+            lvPersonne.Columns.Add("Prénom", 150, HorizontalAlignment.Left);
+            lvPersonne.Columns.Add("Age", 150, HorizontalAlignment.Left);
+            lvPersonne.Columns.Add("Téléphone", 150, HorizontalAlignment.Left);
+            txtNom.Focus();
+
+            string line;
+
+            try
+            {
+                StreamReader sr = new StreamReader(@"personnes.txt");
+                
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        string[] info = line.Split(' ');
+
+                        ListViewItem listviewItem = new ListViewItem();
+                        listviewItem.Text = info[0];
+                        listviewItem.SubItems.Add(info[1] + " " + info[2]);
+                        listviewItem.SubItems.Add(info[3]);
+                        listviewItem.SubItems.Add(info[7]);
+
+                        lvPersonne.Items.Add(listviewItem);
+                    line = sr.ReadLine();
+
+                }
+            }
+            catch(Exception j)
+            {
+                Console.WriteLine(j.Message);
+            }
+
+           
+            
+            
+        }
+
+        
+    }
+}
